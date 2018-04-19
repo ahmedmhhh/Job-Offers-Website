b76626d3a8050faa9dfff0ced0c91f185c886d57
Ahmed <a7medmhhh@hotmail.com>
2018-04-19 14:13:13 +0200

    -create navbar & footer

---
+++ Job-Offers-Website/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Job_Offers_Website.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Job-Offers-Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Job_Offers_Website.MvcApplication" Language="C#" %>
