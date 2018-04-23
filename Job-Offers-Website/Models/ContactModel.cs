51341822db0f3a54ff2681473446c9f15ee35e9c
Ahmed <a7medmhhh@hotmail.com>
2018-04-23 12:16:29 +0200

    -send email

---
+++ Job-Offers-Website/Models/ContactModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Job_Offers_Website.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
